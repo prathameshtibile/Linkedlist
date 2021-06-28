6af2d7238670a8b47a5888c8615c6d7d33713576
Prathamesh Tibile <prathameshtibile1997@gmail.com>
2021-06-28 21:05:57 +0530

    Create a simple Linledlist of 56,30,70

---
+++ Runner.java
@@ -0,0 +1,20 @@
+/**
+ * @author PRATHAMESH TIBILE
+ * @since 26-6-21
+ */
+//************************************************************************
+//	 *  In this code a simple linked list of integers is created, with 3 
+//	 *  int 56,30 and 70.
+//	 * 
+package LinkedList;
+public class Runner {
+	
+		public static void main(String[] args) 
+		{
+			LinkedList<Integer> numberList = new LinkedList<Integer>();
+			numberList.add(56);
+			numberList.add(30);
+			numberList.add(70);
+			numberList.show();
+		}
+}
