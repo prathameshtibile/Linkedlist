d7304f4e5e3374c28840a76aadcfc4e8f7726432
Prathamesh Tibile <prathameshtibile1997@gmail.com>
2021-06-29 13:27:14 +0530

    UC2-program Upload

---
+++ Runner2.java
@@ -0,0 +1,12 @@
+package LinkedList;
+
+public class Runner2 {public static void main(String[] args) 
+{
+	UC2Linklist<Integer> numberList = new UC2Linklist<Integer>();
+	numberList.add(70);
+	numberList.add(30);
+	numberList.add(56);
+	numberList.show();
+}
+
+}
