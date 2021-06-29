96c5a9152e7e2f31b82a979474839ed77a218e4f
Prathamesh Tibile <prathameshtibile1997@gmail.com>
2021-06-29 13:29:57 +0530

    UC3-program Upload

---
+++ Runner3.java
@@ -0,0 +1,11 @@
+package LinkedList;
+
+public class Runner3 {
+	public static void main(String[] args) {
+		UC3Linklist<Integer> numberList = new UC3Linklist<Integer>();
+		numberList.add(56);
+		numberList.add(30);
+		numberList.add(70);
+		numberList.show();
+	}
+}
